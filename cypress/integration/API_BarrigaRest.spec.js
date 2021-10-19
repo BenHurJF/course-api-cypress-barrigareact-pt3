32c63ad31f8a1e9075e45adc1459043683b644b4
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-19 01:20:33 -0300

    definindo uma baseUrl e adicionando os IT's caso de teste

---
+++ cypress/integration/API_BarrigaRest.spec.js
@@ -0,0 +1,27 @@
+/// <reference types="Cypress" />
+
+describe('Testes de API do Barriga Rect', () => {
+    before(() => {
+        // cy.login()
+    })
+
+    it('1- Criando uma conta', () => {
+        
+    })
+
+    it('2- Alterando uma conta', () => {
+        
+    })
+
+    it('3- Tentando criar conta com nome já cadastrado', () => {
+        
+    })
+
+    it('4- criando transação', () => {
+        
+    })
+
+    it('5- Validando saldo', () => {
+        
+    })
+})
