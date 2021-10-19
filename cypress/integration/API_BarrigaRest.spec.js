a4ed4a6888987b5ab87378e5a2be733990dfd23a
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-19 01:55:18 -0300

    add comando customizado para abrir cypress e um Request 'POST' para obtenção de token

--- cypress/integration/API_BarrigaRest.spec.js
+++ cypress/integration/API_BarrigaRest.spec.js
@@ -6,7 +6,23 @@ describe('Testes de API do Barriga Rect', () => {
     })
 
     it('1- Criando uma conta', () => {
-        
+        cy.request({
+            method: 'POST',
+            url: 'signin',
+            failOnStatusCode: true,
+            body: {
+                email: "beiujeffer@hotmail.com",
+                redirecionar: false,
+                senha: "91150510"
+            }
+        }).then(response => {
+            expect(response.status).to.be.eq(200)
+            cy.wrap(response).its('body.id').should('eq', 25541)
+            cy.wrap(response).its('body.nome').should('eq', 'Ben-Hur Jeffer')
+            cy.wrap(response).its('body.token').should('not.be.empty')
+        }).then(response => {
+             
+        })
     })
 
     it('2- Alterando uma conta', () => {
