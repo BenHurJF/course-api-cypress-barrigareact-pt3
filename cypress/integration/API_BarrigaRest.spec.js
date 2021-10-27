8fffe841046571d65299a6349cc4df18ecfdca73
Ben-Hur Jeffer <67138155+BenHurJF@users.noreply.github.com>
2021-10-27 10:36:28 -0300

    validando saldo

--- cypress/integration/API_BarrigaRest.spec.js
+++ cypress/integration/API_BarrigaRest.spec.js
@@ -5,24 +5,24 @@ describe('Testes de API do Barriga Rect', () => {
 
     before(() => {
         cy.getToken('beiujeffer@hotmail.com', '91150510')
-        .then(token => {
-            TOKEN = token
-        })
+            .then(token => {
+                TOKEN = token
+            })
         cy.resetarContas()
     })
 
     beforeEach(() => {
-        
+
     })
 
     it('1- Criando uma conta', () => {
-            cy.request({
+        cy.request({
             url: 'contas',
             method: 'POST',
             failOnStatusCode: true,
-            headers: { Authorization: `JWT ${TOKEN}`},
+            headers: { Authorization: `JWT ${TOKEN}` },
             body: {
-               nome: "Conta para Teste API"
+                nome: "Conta para Teste API"
             }
         }).as('responseCriarConta')
 
@@ -34,26 +34,26 @@ describe('Testes de API do Barriga Rect', () => {
     })
 
     it('2- Alterando uma conta', () => {
-         cy.request({
-             method: 'GET',
-             url: 'contas',
-             headers: { Authorization: `JWT ${TOKEN}` },
-             qs: {
-                 nome: 'Conta para Teste API'
-             }
-         }).then(res => {
-            cy.request({
-            method: 'PUT',
-            url: `contas/${res.body[0].id}`,
-            failOnStatusCode: false,
+        cy.request({
+            method: 'GET',
+            url: 'contas',
             headers: { Authorization: `JWT ${TOKEN}` },
-            body: {
-                nome: 'EDITANDO CONTA PELO API'
+            qs: {
+                nome: 'Conta para Teste API'
             }
-        }).as('response')
-         })
+        }).then(res => {
+            cy.request({
+                method: 'PUT',
+                url: `contas/${res.body[0].id}`,
+                failOnStatusCode: false,
+                headers: { Authorization: `JWT ${TOKEN}` },
+                body: {
+                    nome: 'EDITANDO CONTA PELO API'
+                }
+            }).as('response')
+        })
         cy.get('@response').its('status').should('be.eq', 200) // OK
-        
+
     })
 
     it('3- Tentando criar conta com nome já cadastrado', () => {
@@ -61,9 +61,9 @@ describe('Testes de API do Barriga Rect', () => {
             url: 'contas',
             method: 'POST',
             failOnStatusCode: false,
-            headers: { Authorization: `JWT ${TOKEN}`},
+            headers: { Authorization: `JWT ${TOKEN}` },
             body: {
-               nome: "Conta mesmo nome"
+                nome: "Conta mesmo nome"
             }
         }).as('responseCriarConta')
 
@@ -74,32 +74,76 @@ describe('Testes de API do Barriga Rect', () => {
         })
     })
 
-    it.only('4- criando transação', () => {
+    it('4- criando transação', () => {
         cy.getContaByName('Conta para movimentacoes')
             .then(contaId => {
-           cy.request({
-            method: 'POST',
-            url: 'transacoes',
-            // failOnStatusCode: false,
-            headers: { Authorization: `JWT ${TOKEN}` },
-            body: {
-                conta_id: contaId,
-                data_pagamento: '30/10/2021',
-                data_transacao: '25/10/2021',
-                descricao: "TESTE",
-                envolvido: "EU",
-                status: true,
-                tipo: "REC",
-                valor: "30022"
-            }
-        }).as('resTransacao')
-    })
-    cy.get('@resTransacao').its('status').should('be.equal', 201)
-    cy.get('@resTransacao').its('body.valor').should('exist')
+                cy.request({
+                    method: 'POST',
+                    url: 'transacoes',
+                    // failOnStatusCode: false,
+                    headers: { Authorization: `JWT ${TOKEN}` },
+                    body: {
+                        conta_id: contaId,
+                        data_pagamento: '30/10/2021',
+                        data_transacao: '25/10/2021',
+                        descricao: "TESTE",
+                        envolvido: "EU",
+                        status: true,
+                        tipo: "REC",
+                        valor: "30022"
+                    }
+                }).as('resTransacao')
+            })
+        cy.get('@resTransacao').its('status').should('be.equal', 201)
+        cy.get('@resTransacao').its('body.valor').should('exist')
 
     })
 
     it('5- Validando saldo', () => {
-        
+        cy.request({
+            method: 'GET',
+            url: 'saldo',
+            headers: { Authorization: `JWT ${TOKEN}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.contain('534.00')
+        })
+
+        cy.request({
+            method: 'GET',
+            url: 'transacoes',
+            qs: { descricao: "Movimentacao 1, calculo saldo" },
+            headers: { Authorization: `JWT ${TOKEN}` }
+        }).then(movimentacao => {
+            cy.request({
+                url: `transacoes/${movimentacao.body[0].id}`,
+                method: 'PUT',
+                headers: { Authorization: `JWT ${TOKEN} ` },
+                body: {
+                    status: true,
+                    conta_id: 890968,
+                    data_pagamento: cy.clock(),
+                    data_transacao: cy.clock(),
+                    descricao: movimentacao.body[0].descricao,
+                    envolvido: movimentacao.body[0].envolvido,
+                    valor: movimentacao.body[0].valor
+                }
+            }).its('status').should('be.equal', 200)
+        })
+
+        cy.request({
+            method: 'GET',
+            url: 'saldo',
+            headers: { Authorization: `JWT ${TOKEN}` }
+        }).then(res => {
+            let saldoConta = null
+            res.body.forEach(c => {
+                if (c.conta === 'Conta para saldo') saldoConta = c.saldo
+            })
+            expect(saldoConta).to.be.contain('3.500')
+        })
     })
 })
